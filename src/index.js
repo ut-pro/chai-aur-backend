ef60a5cdf086f2d9613540dd2d1c4bd15cc95a8c
ut-pro <utkarsh.singh144493@gmail.com>
2025-03-29 02:23:19 +0530

    set up Express server and update package.json dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import dotenv from "dotenv";
 
+dotenv.config();
+
 import connectDB from "./db/db.js";
 
-dotenv.config({
-  path: "./env",
-});
+import app from "./app.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, (err) => {
+      if (err) console.log("Server not connected");
+      else
+        console.log(
+          `Server is connected at http://localhost:${process.env.PORT}`
+        );
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed", err);
+  });
